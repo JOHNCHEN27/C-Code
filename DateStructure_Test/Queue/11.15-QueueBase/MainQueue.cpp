53b3d34b79fa4ad71539194b7cd2877a6d2c0f4d
JOHNCHEN27 <2502523383@qq.com>
2023-07-21 15:09:32 +0800

    Git操作

--- DateStructure_Test/Queue/11.15-QueueBase/MainQueue.cpp
+++ DateStructure_Test/Queue/11.15-QueueBase/MainQueue.cpp
@@ -2,7 +2,7 @@
 
 int main()
 {
-	Queue s;
+	/*Queue s;
 	QueueInit(&s);
 	QueuePush(&s, 11);
 	QueuePush(&s, 23);
@@ -22,6 +22,26 @@ int main()
 
 
 
-	QueueDestroy(&s);
+	QueueDestroy(&s);*/
+
+	int arr[] = { 2,35,64,23,653,123 };
+	int len = sizeof(arr) / sizeof(int);
+	for (int i = 0; i < len; i++)
+	{
+		for (int j = i + 1; j >= 0; j--)
+		{
+			if (arr[j - 1] > arr[j])
+			{
+				int temp = arr[j - 1];
+				arr[j - 1] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+
+	for (int i = 0; i < len; i++)
+	{
+		cout << arr[i] << " ";
+	}
 	return 0;
 }
